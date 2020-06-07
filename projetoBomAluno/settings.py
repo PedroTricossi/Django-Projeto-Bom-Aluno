7bdd45a346fab1058b447afc719805ccafe2b762
Pedro Tricossi <pedrodtricossi@gmail.com>
2020-06-07 16:50:03 -0300

    page app added and tested, 2-15

--- projetoBomAluno/settings.py
+++ projetoBomAluno/settings.py
@@ -19,7 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users.apps.UsersConfig'
+    #local apps
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -37,7 +39,7 @@ ROOT_URLCONF = 'projetoBomAluno.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
