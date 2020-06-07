2920734995509a991d966d456e171c825840b405
Pedro Tricossi <pedrodtricossi@gmail.com>
2020-06-07 17:23:23 -0300

    Logins func added and tested, 3-15

--- pages/tests.py
+++ pages/tests.py
@@ -17,7 +17,7 @@ class HomepageTests(SimpleTestCase):
         self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_contains_correct_html(self):
-        self.assertContains(self.response, 'Homepage')
+        self.assertContains(self.response, 'Esse é o inicio do Projeto Bom Aluno')
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(
