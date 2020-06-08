4d64f7f37068b767454fde30ae1a19d38bf762a5
Pedro Tricossi <pedrodtricossi@gmail.com>
2020-06-08 15:57:09 -0300

    make it better

--- projetoBomAluno/settings.py
+++ projetoBomAluno/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.herokuapp.com', '192.168.99.100']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
