d0abd353072366d27a8f3e4d1079486007d20e28
Pedro Tricossi <pedrodtricossi@gmail.com>
2020-06-07 22:23:51 -0300

    search bar is working!!!, 12-15

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import StudentListView, StudentDetailView
+from .views import StudentListView, StudentDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', StudentListView.as_view(), name='student_list'),
     path('<uuid:pk>', StudentDetailView.as_view(), name='student_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results')
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from .models import Student
@@ -11,4 +12,16 @@ class StudentListView(ListView):
 class StudentDetailView(DetailView):
     model = Student
     context_object_name = 'student'
-    template_name = 'students/srudent_detail.html'
+    template_name = 'students/student_detail.html'
+
+class SearchResultsListView(ListView):
+    model = Student
+    context_object_name = 'student_list'
+    template_name = 'students/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Student.objects.filter(
+            Q(name__icontains=query) | Q(course__icontains=query)
+        )
+    
